69d99ce70d4fdbabfd7952c70bda62613cf9c524
Sergey Fesenko <sergey.fesenko@accedo.tv>
2016-06-19 15:12:22 +0800

    update the prices

--- src/js/prices.js
+++ src/js/prices.js
@@ -1,5 +1,5 @@
 (function () {
     $('#download-btn').on('click', function() {
-        location.assign('/doc/multipack_price_2016.doc');
+        location.assign('/doc/multipack-price_april-2016.doc');
     });
 })();
